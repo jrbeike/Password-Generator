0f07e449bd002b1c6de516065ccf1d2cfbc88599
jrbeike <jrbeike@gmail.com>
2022-02-27 16:14:09 -0500

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,118 @@
 // Assignment code here
 
+// array of special characheter to be included in password. 
+var specialCharacters = [
+    '@',
+    '!',
+    '#',
+    '$',
+    '%',
+    '^',
+    '&',
+    '*',
+    '(',
+    ')',
+    '[',
+    ']',
+    '{',
+    '}',
+    '\',
+    '~',
+    '+',
+    '?',
+    ',',
+    ':',
+    ';',
+    '_',
+    '-',
+    '/',
+];
+
+// array of numerical characheters to be in password
+var numericCharaters = [
+    '1',
+    '2',
+    '3',
+    '4',
+    '5',
+    '6',
+    '7',
+    '8',
+    '9',
+    '0',
+]
+
+// Array of letters to be in the password
+var lowerCasedCharacters = [
+    'a',
+    'b',
+    'c',
+    'd',
+    'e',
+    'f',
+    'g',
+    'h',
+    'i',
+    'j',
+    'k',
+    'l',
+    'm',
+    'n',
+    'o',
+    'p',
+    'q',
+    'r',
+    's',
+    't',
+    'u',
+    'v',
+    'w',
+    'x',
+    'y',
+    'z',
+]
+
+var upperCasedCharacters = [
+    'A',
+    'B',
+    'C',
+    'D',
+    'E',
+    'F',
+    'G',
+    'H',
+    'I',
+    'J',
+    'K',
+    'L',
+    'M',
+    'N',
+    'O',
+    'P',
+    'Q',
+    'R',
+    'S',
+    'T',
+    'U',
+    'V',
+    'W',
+    'X',
+    'Y',
+    'Z',
+]
+
+// PROMPT FOR USER FOR PASSOWORD OPTIONS
+function getPasswordOptions(){
+    var length = parseInt(
+        prompt('How mant characters would you like you password to contain (8-128)')
+    );
+    //cOntditional to check to make sure it is a numeber
+    if (Number.isNaN(length)) {
+        alert('password length must be provided as a NUMBER');
+        return null;
+    }
+
+}
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
